0154e8343fe808346a38694ff26e88a25a725acb
Mandebass <seuriusbassman@gmail.com>
2025-03-31 21:26:47 +0200

    header logo hozzáadva

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from 'app/styles/header.module.css';
 import Image from "next/image";
 
+import Logo from '@/app/public/images/header.png'
+
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <p>E.jon</p>
+        <Image src={Logo} alt="Logo" width={200} />
         <nav id="navbar">
           <a href="/" className={styles.navLink}>Kezdőlap</a>
           <a href="/login" className={styles.navLink}>Bejelentkezés</a>
